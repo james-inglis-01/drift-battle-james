d402d553ae80953817a7b94993be5b88cd32a765
James Inglis <james.a.i.inglis@gmail.com>
2017-08-21 12:07:36 +1200

    Formatting files

--- public/bundle.js
+++ public/bundle.js
@@ -9902,44 +9902,44 @@ module.exports = __webpack_require__(117);
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 var cardata = [{
-    id: 1,
-    make: 'Mazda',
-    model: 'RX-7',
-    image: 'RX-7.jpg',
-    horsePower: 276
+  id: 1,
+  make: 'Mazda',
+  model: 'RX-7',
+  image: 'RX-7.jpg',
+  horsePower: 276
 }, {
-    id: 2,
-    make: 'Nissan',
-    model: 'Silvia S15',
-    image: 'Silvia-S15.jpg',
-    horsePower: 250
+  id: 2,
+  make: 'Nissan',
+  model: 'Silvia S15',
+  image: 'Silvia-S15.jpg',
+  horsePower: 250
 }, {
-    id: 3,
-    make: 'Toyota',
-    model: 'Corolla AE-86',
-    image: 'AE-86.jpg',
-    horsePower: 112
+  id: 3,
+  make: 'Toyota',
+  model: 'Corolla AE-86',
+  image: 'AE-86.jpg',
+  horsePower: 112
 }, {
-    id: 4,
-    make: 'Nissan',
-    model: '350Z',
-    image: 'rocket-bunny-z.jpg',
-    horsePower: 276
+  id: 4,
+  make: 'Nissan',
+  model: '350Z',
+  image: 'rocket-bunny-z.jpg',
+  horsePower: 276
 }, {
-    id: 5,
-    make: 'Toyota',
-    model: 'Supra',
-    image: 'Supra.jpg',
-    horsePower: 320
+  id: 5,
+  make: 'Toyota',
+  model: 'Supra',
+  image: 'Supra.jpg',
+  horsePower: 320
 }, {
-    id: 6,
-    make: 'Mazda',
-    model: 'MX-5',
-    image: 'Mazda-MX-5.jpg',
-    horsePower: 115
+  id: 6,
+  make: 'Mazda',
+  model: 'MX-5',
+  image: 'Mazda-MX-5.jpg',
+  horsePower: 115
 }];
 
 exports.default = cardata;
